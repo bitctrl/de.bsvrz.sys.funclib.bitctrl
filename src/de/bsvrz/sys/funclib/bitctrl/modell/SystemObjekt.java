7ca891132ed9c8c44f9d0adcfd80bf7512710e75
Schumann <Falko.Schumann@bitctrl.de>
2007-10-05 14:37:53 +0000

    Kommentare ergänzt.
    git-svn-id: svn://ntoserver/Datenverteiler/VRZ3/trunk/de.bsvrz.sys.funclib.bitctrl@4129 40c0be50-5d29-0410-901d-fb59edb5cafc

--- src/de/bsvrz/sys/funclib/bitctrl/modell/SystemObjekt.java
+++ src/de/bsvrz/sys/funclib/bitctrl/modell/SystemObjekt.java
@@ -1,20 +1,20 @@
 /*
- * Segment 5 Intelligente Analyseverfahren, SWE 5.2 Straﬂensubsegmentanalyse
- * Copyright (C) 2007 BitCtrl Systems GmbH
+ * Allgemeine Funktionen mit und ohne Datenverteilerbezug
+ * Copyright (C) 2007 BitCtrl Systems GmbH 
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
  *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
+ * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
  * details.
  *
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc., 51
- * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
  *
  * Contact Information:
  * BitCtrl Systems GmbH
@@ -34,20 +34,10 @@ import de.bsvrz.dav.daf.main.config.SystemObject;
  * Datenverteilsers zur&uuml;ck gibt. Die anderen sollen lediglich die
  * Schreibweise verk&uuml;rzen.
  * 
- * @author BitCtrl, Schumann
+ * @author BitCtrl Systems GmbH, Falko Schumann
  * @version $Id$
  */
 public interface SystemObjekt {
-
-	// /**
-	// * Gibt den Konfigurationsbereich zur&uuml;ck in sich das Systemobjekt
-	// * befindet.
-	// *
-	// * @see
-	// stauma.dav.configuration.interfaces.SystemObject#getConfigurationArea()
-	// * @return Der Konfigurationsbereich
-	// */
-	// ConfigurationArea getKonfigurationsbereich();
 
 	/**
 	 * Gibt die ID des Systemobjekts zur&uuml;ck.
@@ -58,21 +48,6 @@ public interface SystemObjekt {
 	long getId();
 
 	/**
-	 * Gibt die PID des Systemobjekts zur&uuml;ck.
-	 * 
-	 * @see SystemObject#getPid()
-	 * @return Die PID als String
-	 */
-	String getPid();
-
-	/**
-	 * Gibt den Typ des Systemobjekts zur&uuml;ck.
-	 * 
-	 * @return den Typ
-	 */
-	SystemObjektTyp getTyp();
-
-	/**
 	 * Gibt den Namen des Systemobjekts zur&uuml;ck.
 	 * 
 	 * @see SystemObject#getName()
@@ -81,11 +56,26 @@ public interface SystemObjekt {
 	String getName();
 
 	/**
+	 * Gibt die PID des Systemobjekts zur&uuml;ck.
+	 * 
+	 * @see SystemObject#getPid()
+	 * @return Die PID als String
+	 */
+	String getPid();
+
+	/**
 	 * Gibt das gekapselte Systemobjekt des Datenverteilers zur&uuml;ck.
 	 * 
 	 * @see SystemObject#getId()
 	 * @return Das Datenverteilersystemobjekt
 	 */
 	SystemObject getSystemObject();
+
+	/**
+	 * Gibt den Typ des Systemobjekts zur&uuml;ck.
+	 * 
+	 * @return den Typ
+	 */
+	SystemObjektTyp getTyp();
 
 }
