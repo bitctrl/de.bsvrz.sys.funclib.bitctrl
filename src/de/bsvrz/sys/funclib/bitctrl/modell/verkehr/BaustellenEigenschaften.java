8c7f0125c974bcf1467f7bb0944a85c6a0faada0
peuker <uwe.peuker@bitctrl.de>
2007-10-11 18:48:52 +0000

    git-svn-id: svn://ntoserver/Datenverteiler/VRZ3/trunk/de.bsvrz.sys.funclib.bitctrl@4315 40c0be50-5d29-0410-901d-fb59edb5cafc

---
+++ src/de/bsvrz/sys/funclib/bitctrl/modell/verkehr/BaustellenEigenschaften.java
@@ -0,0 +1,61 @@
+package de.bsvrz.sys.funclib.bitctrl.modell.verkehr;
+
+import de.bsvrz.dav.daf.main.Data;
+import de.bsvrz.dav.daf.main.config.AttributeGroup;
+import de.bsvrz.sys.funclib.bitctrl.modell.AbstractParameterDatensatz;
+import de.bsvrz.sys.funclib.bitctrl.modell.SystemObjekt;
+
+public class BaustellenEigenschaften extends AbstractParameterDatensatz {
+
+	private static AttributeGroup attributGruppe;
+
+	/**
+	 * Zustand der Baustelle. ("Status")
+	 */
+	private BaustellenStatus status;
+
+	/**
+	 * Restkapazität während der Gültigkeitsdauer der Baustelle.
+	 * ("RestKapazität")
+	 */
+	private long restKapazitaet;
+
+	/**
+	 * Veranlasser der Baustelle (BIS-System oder VRZ). ("Veranlasser")
+	 */
+	private BaustellenVeranlasser veranlasser;
+
+	public BaustellenEigenschaften(SystemObjekt objekt) {
+		super(objekt);
+		if (attributGruppe == null) {
+			attributGruppe = objekt.getSystemObject().getDataModel()
+					.getAttributeGroup("atg.baustellenEigenschaften");
+		}
+	}
+
+	public AttributeGroup getAttributGruppe() {
+		return attributGruppe;
+	}
+
+	public long getRestKapazitaet() {
+		return restKapazitaet;
+	}
+
+	public BaustellenStatus getStatus() {
+		return status;
+	}
+
+	public BaustellenVeranlasser getVeranlasser() {
+		return veranlasser;
+	}
+
+	public void setDaten(Data daten) {
+		if (daten != null) {
+			status = BaustellenStatus.getStatus(daten
+					.getUnscaledValue("Status").intValue());
+			restKapazitaet = daten.getScaledValue("RestKapazität").longValue();
+			veranlasser = BaustellenVeranlasser.getVeranlasser(daten
+					.getUnscaledValue("Veranlasser").intValue());
+		}
+	}
+}
---
+++ src/de/bsvrz/sys/funclib/bitctrl/modell/verkehr/SituationsEigenschaften.java
@@ -0,0 +1,93 @@
+package de.bsvrz.sys.funclib.bitctrl.modell.verkehr;
+
+import java.util.List;
+
+import de.bsvrz.dav.daf.main.Data;
+import de.bsvrz.dav.daf.main.config.AttributeGroup;
+import de.bsvrz.sys.funclib.bitctrl.modell.AbstractParameterDatensatz;
+import de.bsvrz.sys.funclib.bitctrl.modell.ObjektFactory;
+import de.bsvrz.sys.funclib.bitctrl.modell.SystemObjekt;
+
+public class SituationsEigenschaften extends AbstractParameterDatensatz {
+
+	/**
+	 * die Attributgruppe f¸r den Zugriff auf die Parameter.
+	 */
+	private static AttributeGroup attributGruppe;
+	/**
+	 * Startzeitpunkt der Situation (Staubeginn, Baustellenbeginn, // etc.).
+	 * ("StartZeit")
+	 */
+	private long startZeit;
+
+	/**
+	 * Dauer des Situation (sofern bekannt). Eintrag von 0 ms bedeutet //
+	 * unbekannte (unendliche) Dauer. ("Dauer")
+	 */
+	private long dauer;
+
+	/**
+	 * Referenzen auf alle Straﬂensegmente, ¸ber die sich die Situation
+	 * ausbreitet. ("StraﬂenSegment")
+	 */
+	private List<StrassenSegment> segmente;
+
+	/**
+	 * Position des Situationsanfangs im ersten Straﬂensegment. ("StartOffset")
+	 */
+	long startOffset;
+
+	/**
+	 * Position des Situationsendes im letzten Straﬂensegment. ("EndOffset")
+	 */
+	long endOffset;
+
+	public SituationsEigenschaften(SystemObjekt objekt) {
+		super(objekt);
+		if (attributGruppe == null) {
+			attributGruppe = objekt.getSystemObject().getDataModel()
+					.getAttributeGroup("atg.situationsEigenschaften");
+		}
+	}
+
+	public AttributeGroup getAttributGruppe() {
+		return attributGruppe;
+	}
+
+	public long getDauer() {
+		return dauer;
+	}
+
+	public long getEndOffset() {
+		return endOffset;
+	}
+
+	public List<StrassenSegment> getSegmente() {
+		return segmente;
+	}
+
+	public long getStartOffset() {
+		return startOffset;
+	}
+
+	public long getStartZeit() {
+		return startZeit;
+	}
+
+	public void setDaten(Data daten) {
+		if (daten != null) {
+			startZeit = daten.getTimeValue("StartZeit").getMillis();
+			dauer = daten.getUnscaledValue("Dauer").longValue();
+			segmente.clear();
+			Data.Array segmentArray = daten.getArray("StraﬂenSegment");
+			for (int idx = 0; idx < segmentArray.getLength(); idx++) {
+				segmente.add((StrassenSegment) ObjektFactory.getInstanz()
+						.getModellobjekt(
+								segmentArray.getReferenceValue(idx)
+										.getSystemObject()));
+			}
+			startOffset = daten.getUnscaledValue("StartOffset").longValue();
+			endOffset = daten.getUnscaledValue("EndOffset").longValue();
+		}
+	}
+}
