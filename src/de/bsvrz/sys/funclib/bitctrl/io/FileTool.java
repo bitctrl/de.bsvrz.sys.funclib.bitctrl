6e5f83b5634db1543a4e6281fc052e72b0ffbb73
peuker <uwe.peuker@bitctrl.de>
2008-02-28 08:49:19 +0000

    ThreadMX entfernt
    git-svn-id: svn://ntoserver/Datenverteiler/VRZ3/trunk/de.bsvrz.sys.funclib.bitctrl@7032 40c0be50-5d29-0410-901d-fb59edb5cafc

--- src/de/bsvrz/sys/funclib/bitctrl/io/FileTool.java
+++ src/de/bsvrz/sys/funclib/bitctrl/io/FileTool.java
@@ -31,8 +31,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.management.ManagementFactory;
-import java.lang.management.ThreadInfo;
 
 /**
  * Allgemeine Funktionen zur Arbeit mit Dateien.
@@ -174,24 +172,8 @@ public final class FileTool {
 	 *            die Argumente
 	 */
 	public static void main(String[] args) {
-		ThreadInfo[] infos = ManagementFactory.getThreadMXBean()
-				.dumpAllThreads(true, true);
-		for (ThreadInfo info : infos) {
-			System.err.println(info.getThreadName()
-					+ ": "
-					+ ManagementFactory.getThreadMXBean().getThreadCpuTime(
-							info.getThreadId()) / 1000000 + " ms");
-		}
 		FileTool.kopiereVerzeichnis(new File("H:/TEMP/Neuer Ordner"), new File(
 				"H:/TEMP/TESTKOPIE"));
-
-		for (ThreadInfo info : infos) {
-			System.err.println(info.getThreadName()
-					+ ": "
-					+ ManagementFactory.getThreadMXBean().getThreadCpuTime(
-							info.getThreadId()) / 1000000 + " ms");
-		}
-
 	}
 
 	/**
