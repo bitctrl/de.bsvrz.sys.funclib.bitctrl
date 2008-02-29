5745afcc326d931a360a558e525a8d020b006f34
Schumann <Falko.Schumann@bitctrl.de>
2008-02-29 13:41:30 +0000

    NEU: DatenSendeException bei Timeout gibt den letzten Status der Sendesteuerung mit aus.
    git-svn-id: svn://ntoserver/Datenverteiler/VRZ3/trunk/de.bsvrz.sys.funclib.bitctrl@7082 40c0be50-5d29-0410-901d-fb59edb5cafc

--- src/de/bsvrz/sys/funclib/bitctrl/modell/AbstractDatensatz.java
+++ src/de/bsvrz/sys/funclib/bitctrl/modell/AbstractDatensatz.java
@@ -368,6 +368,47 @@ public abstract class AbstractDatensatz<T extends Datum> implements
 	}
 
 	/**
+	 * Zwei Datens&auml;tze sind gleich, wenn sie die selbe Attributgruppe am
+	 * gleichen Systemobjekt abbilden.
+	 * <p>
+	 * {@inheritDoc}
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(final Object obj) {
+		if (obj == this) {
+			return true;
+		}
+		if (obj instanceof Datensatz) {
+			Datensatz<?> ds = (Datensatz<?>) obj;
+			return getObjekt().equals(ds.getObjekt())
+					&& getAttributGruppe().equals(ds.getAttributGruppe());
+		}
+		return false;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see de.bsvrz.sys.funclib.bitctrl.modell.Datensatz#getObjekt()
+	 */
+	public SystemObjekt getObjekt() {
+		return objekt;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return getClass().getName() + "[objekt=" + getObjekt() + ", daten="
+				+ daten + "]";
+	}
+
+	/**
 	 * Meldet eine eventuell vorhandene Anmeldung als Sender oder Quelle wieder
 	 * ab. Noch nicht gesendet Datens&auml;tze werden aus dem Sendepuffer
 	 * entfernt.
@@ -480,27 +521,6 @@ public abstract class AbstractDatensatz<T extends Datum> implements
 	}
 
 	/**
-	 * Zwei Datens&auml;tze sind gleich, wenn sie die selbe Attributgruppe am
-	 * gleichen Systemobjekt abbilden.
-	 * <p>
-	 * {@inheritDoc}
-	 * 
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(final Object obj) {
-		if (obj == this) {
-			return true;
-		}
-		if (obj instanceof Datensatz) {
-			Datensatz<?> ds = (Datensatz<?>) obj;
-			return getObjekt().equals(ds.getObjekt())
-					&& getAttributGruppe().equals(ds.getAttributGruppe());
-		}
-		return false;
-	}
-
-	/**
 	 * Gibt einen leeren Sendecache zur&uuml;ck.
 	 * 
 	 * @return ein leeres {@code Data}.
@@ -551,15 +571,6 @@ public abstract class AbstractDatensatz<T extends Datum> implements
 	 */
 	protected T getDatum(final Aspect asp) {
 		return daten.get(asp);
-	}
-
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @see de.bsvrz.sys.funclib.bitctrl.modell.Datensatz#getObjekt()
-	 */
-	public SystemObjekt getObjekt() {
-		return objekt;
 	}
 
 	/**
@@ -693,7 +704,8 @@ public abstract class AbstractDatensatz<T extends Datum> implements
 				}
 			}
 			if (getStatusSendesteuerung(asp) != Datensatz.Status.START) {
-				throw new DatensendeException("Timeout (" + this + ")");
+				throw new DatensendeException("Timeout, Sendesteuerung="
+						+ getStatusSendesteuerung(asp) + " (" + this + ")");
 			}
 			sender.sende(konvertiere(datum), asp, datum.getZeitstempel());
 		}
@@ -712,17 +724,6 @@ public abstract class AbstractDatensatz<T extends Datum> implements
 	 */
 	protected void setDatum(final Aspect asp, final T datum) {
 		daten.put(asp, datum);
-	}
-
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return getClass().getName() + "[objekt=" + getObjekt() + ", daten="
-				+ daten + "]";
 	}
 
 	/**
