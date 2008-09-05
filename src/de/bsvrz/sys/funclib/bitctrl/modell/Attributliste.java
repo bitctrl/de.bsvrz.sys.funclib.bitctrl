60cc25e6855fe3ada9cf571c6d310b2a444f22a5
Schumann <Falko.Schumann@bitctrl.de>
2008-09-05 15:44:34 +0000

    NEU: Schnittstelle für Attributlisten.
    git-svn-id: svn://ntoserver/Datenverteiler/VRZ3/trunk/de.bsvrz.sys.funclib.bitctrl@11745 40c0be50-5d29-0410-901d-fb59edb5cafc

---
+++ src/de/bsvrz/sys/funclib/bitctrl/modell/Attributliste.java
@@ -0,0 +1,59 @@
+/*
+ * Allgemeine Funktionen mit und ohne Datenverteilerbezug
+ * Copyright (C) 2007 BitCtrl Systems GmbH 
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ *
+ * Contact Information:
+ * BitCtrl Systems GmbH
+ * Weißenfelser Straße 67
+ * 04229 Leipzig
+ * Phone: +49 341-490670
+ * mailto: info@bitctrl.de
+ */
+
+package de.bsvrz.sys.funclib.bitctrl.modell;
+
+import de.bsvrz.dav.daf.main.Data;
+
+/**
+ * Schnittstelle für Attributlisten. Anstelle von zig Setter und Getter für ein
+ * Datensatzdatum sollten die Attributlisten auch auf Objekte abgebildet werden.
+ * Diese Schnittstelle erlaubt die Konvertierung der Daten zwischen einer
+ * Dav-Attributliste und einem Java-Objekt.
+ * 
+ * @author BitCtrl Systems GmbH, Schumann
+ * @version $Id$
+ */
+public interface Attributliste {
+
+	/**
+	 * Schreibt die Daten des Java-Objekts in die übergebenen Attributliste.
+	 * 
+	 * @param daten
+	 *            die zum Objekt korrespondierende Attributliste.
+	 */
+	void extrahiereDaten(final Data daten);
+
+	/**
+	 * Liest die Daten aus der übergebenen Attributliste und übernimmt sie in
+	 * das Java-Objekt.
+	 * 
+	 * @param daten
+	 *            die zum Objekt korrespondierende Attributliste.
+	 */
+	void setDaten(final Data daten);
+
+}
